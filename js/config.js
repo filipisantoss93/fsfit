a735d6a6bafef0a134966585c99b815418d5b881
filipisantoss93 <filipi.199320@gmail.com>
2026-07-14 23:08:10 -0300

    Configura cliente Supabase

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://jjpijncxlkwutbnkpsaw.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_eU3QNbS5L0EqnK8H39XUgw_B2efuOvo';
+
+window.fsSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
