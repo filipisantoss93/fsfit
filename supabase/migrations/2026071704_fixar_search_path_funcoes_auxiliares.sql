5024d357c7ab48017528ef645fb8b2c608a8c6f0
filipisantoss93 <filipi.199320@gmail.com>
2026-07-17 00:37:50 -0300

    Versionar hardening de search_path das funções auxiliares

---
+++ supabase/migrations/2026071704_fixar_search_path_funcoes_auxiliares.sql
@@ -0,0 +1,4 @@
+alter function public.touch_contato_suporte_updated_at() set search_path = public, pg_temp;
+alter function public.fsfit_ajustar_ordem_refeicao() set search_path = public, pg_temp;
+alter function public.marcar_whatsapp_pendente_apos_push() set search_path = public, pg_temp;
+alter function public.registrar_notificacao_whatsapp_ambos() set search_path = public, pg_temp;
