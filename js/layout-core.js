372af1e8f1becb573f113dde3981e2886b8ad72e
filipisantoss93 <filipi.199320@gmail.com>
2026-07-21 20:54:15 -0300

    Finalizar refinamentos da experiência mobile

--- js/layout-core.js
+++ js/layout-core.js
@@ -1,2 +1,3 @@
 import './mobile-experience.js?v=20260721-mobile-polish1';
+import './mobile-experience-fixes.js?v=20260721-mobile-polish2';
 export * from 'https://cdn.jsdelivr.net/gh/filipisantoss93/fsfit@9168e0e760f187b8f8d78a833122397c0a19b934/js/layout.js';
