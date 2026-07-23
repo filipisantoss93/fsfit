cb3739c83036f5174bbe4b79637fcd210dbbeee2
filipisantoss93 <filipi.199320@gmail.com>
2026-07-23 20:20:56 -0300

    Forçar nova versão dos módulos corrigidos da agenda

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -176,13 +176,10 @@ if (document.querySelector('#today-list') || document.querySelector('#live-stude
   import('./painel-agenda-modal.js?v=20260723-agenda-dashboard2').catch(error => {
     console.error('Falha ao carregar dashboard da agenda de hoje:', error);
   });
-  import('./painel-agenda-modal-hotfix.js?v=20260723-agenda-modal-hotfix1').catch(error => {
+  import('./painel-agenda-modal-hotfix.js?v=20260723-agenda-modal-hotfix2').catch(error => {
     console.error('Falha ao carregar correção do modal da agenda de hoje:', error);
   });
-  import('./painel-agenda-modal-avatar.js?v=20260723-agenda-avatar2').catch(error => {
+  import('./painel-agenda-modal-avatar.js?v=20260723-agenda-avatar3').catch(error => {
     console.error('Falha ao carregar a foto do aluno no modal da agenda de hoje:', error);
-  });
-  import('./painel-swipe-tabs.js?v=20260721-swipe-tabs5').catch(error => {
-    console.error('Falha ao carregar gesto lateral entre abas do painel:', error);
   });
 }
