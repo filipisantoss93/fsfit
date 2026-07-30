0453a18db62df123d3cca58e3f61202beb582f36
filipisantoss93 <filipi.199320@gmail.com>
2026-07-30 20:59:52 -0300

    Criar runtime consolidado da biblioteca

---
+++ js/biblioteca-runtime.js
@@ -0,0 +1,5 @@
+import './biblioteca-exercicios.js?v=20260720-category-label1';
+import './biblioteca-treinos.js?v=20260720-workout-builder2';
+import './biblioteca-treinos-multiselect.js?v=20260721-picker-sheet2';
+import './biblioteca-treinos-layout.js?v=20260727-actionbar1';
+import './library-dashboard-desktop.js?v=20260727-library-dashboard1';
