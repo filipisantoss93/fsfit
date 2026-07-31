34a93045fcc840a609318e620139f2024d0ede6e
filipisantoss93 <filipi.199320@gmail.com>
2026-07-30 21:47:47 -0300

    Consolidar runtime da dieta do aluno

---
+++ js/dieta-runtime.js
@@ -0,0 +1,4 @@
+import './dieta-aluno.js';
+import './dieta-biblioteca-refeicoes.js?v=20260716-1';
+import './dieta-modelos-aluno.js?v=20260716-1';
+import './dieta-refeicao-modal.js?v=20260730-runtime1';
