d0c3e8a6ac90ca4d4a34b4f978f80d4e7259835c
filipisantoss93 <filipi.199320@gmail.com>
2026-07-30 22:13:50 -0300

    Criar runtime único da biblioteca alimentar

---
+++ js/biblioteca-alimentar-runtime.js
@@ -0,0 +1,3 @@
+import './biblioteca-alimentar.js?v=20260716-1';
+import './biblioteca-refeicoes-enhancements.js?v=20260718-meals1';
+import './library-dashboard-desktop.js?v=20260727-library-dashboard1';
