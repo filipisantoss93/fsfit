ae5cb84fb137b2b1ba3ee7d49c2dcd284afe81e8
filipisantoss93 <filipi.199320@gmail.com>
2026-07-31 23:26:50 -0300

    Integrar SVGs automáticos aos exercícios

--- js/treino-aluno-runtime.js
+++ js/treino-aluno-runtime.js
@@ -5,3 +5,4 @@ import './treino-multidias.js?v=20260730-rules1';
 import './treino-busca-exercicios.js?v=20260719-search2';
 import './treino-aluno-semanal.js?v=20260716-weekdays1';
 import './exercicio-drag-order.js?v=20260720-dnd1';
+import './exercise-svg-icons.js?v=20260731-svg1';
