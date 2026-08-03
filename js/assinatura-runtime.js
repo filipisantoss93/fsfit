3e703fef45c9e25d58a471927fc959ac704dd77e
filipisantoss93 <filipi.199320@gmail.com>
2026-08-03 19:50:39 -0300

    Integrar realtime ao runtime da assinatura

--- js/assinatura-runtime.js
+++ js/assinatura-runtime.js
@@ -2,4 +2,5 @@
 import './assinatura.js?v=20260725-cancel-pix1';
 import './assinatura-gerenciamento.js?v=20260720-compact1';
 import './assinatura-experiencia.js?v=20260725-contextual1';
+import './assinatura-realtime.js?v=20260803-realtime1';
 import './pwa-install.js';
